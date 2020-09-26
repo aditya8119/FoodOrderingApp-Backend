d05bf87e0bd9a276704b577e2d433e67a26bf0cd
soumyarout80 <soumyarout80@gmail.com>
2020-09-26 21:01:59 +0530

    Added ManyToMany relation with address and customer

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/CustomerEntity.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/CustomerEntity.java
@@ -1,68 +1,75 @@
 package com.upgrad.FoodOrderingApp.service.entity;
 
-import org.apache.commons.lang3.builder.*;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+@NamedQueries({
+        @NamedQuery(name = "customerByPhoneNumber", query = "select c from CustomerEntity c where c.contactNumber = :contact_number"),
+        @NamedQuery(name = "authenticateUserQuery", query = "select c from CustomerEntity c where c.contactNumber= :contactNumber and c.password= :password") })
 @Entity
-@Table(name = "customer", schema = "public")
-@NamedQueries(
-        {
-                @NamedQuery(name = "customerByContact_number", query = "select u from CustomerEntity u where u.contact_number = :contact_number"),
-                @NamedQuery(name = "customerById", query = "select u from CustomerEntity u where u.id = :id")
-        }
-)
-
+@Table(name = "customer")
 public class CustomerEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     @Id
     @Column(name = "ID")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private long id;
 
     @Column(name = "UUID")
-    @NotNull
-    @Size(max = 200)
     private String uuid;
 
-    @Column(name = "FIRSTNAME")
+    @Column(name = "firstname")
     @NotNull
-    @Size(max = 30)
     private String firstName;
 
-    @Column(name = "LASTNAME")
-    @Size(max = 30)
+    @Column(name = "lastname")
+    @NotNull
     private String lastName;
 
-    @Column(name = "EMAIL",unique = true)
+    @Column(name = "email")
     @NotNull
-    @Size(max = 50)
     private String email;
 
-    @Column(name = "CONTACT_NUMBER")
-    @Size(max = 30)
-    private String contact_number;
-
-    @ToStringExclude
-    @Column(name = "PASSWORD")
-    @NotNull
-    @Size(max = 255)
+    @Column(name = "password")
     private String password;
 
-    @ToStringExclude
     @Column(name = "SALT")
     @NotNull
-    @Size(max = 255)
+    @Size(max = 200)
     private String salt;
 
-    public Integer getId() {
+    @Column(name = "contact_number")
+    @NotNull
+    @Size(max = 200)
+    private String contactNumber;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<AddressEntity> address = new ArrayList<AddressEntity>();
+
+    public List<AddressEntity> getAddress() {
+        return address;
+    }
+
+    public void setAddress(List<AddressEntity> address) {
+        this.address = address;
+    }
+
+    public long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(long id) {
         this.id = id;
     }
 
@@ -98,14 +105,6 @@ public class CustomerEntity implements Serializable {
         this.email = email;
     }
 
-    public String getContact_number() {
-        return contact_number;
-    }
-
-    public void setContact_number(String contact_number) {
-        this.contact_number = contact_number;
-    }
-
     public String getPassword() {
         return password;
     }
@@ -122,6 +121,18 @@ public class CustomerEntity implements Serializable {
         this.salt = salt;
     }
 
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
+
     @Override
     public boolean equals(Object obj) {
         return new EqualsBuilder().append(this, obj).isEquals();
@@ -136,5 +147,4 @@ public class CustomerEntity implements Serializable {
     public String toString() {
         return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
     }
-
 }
