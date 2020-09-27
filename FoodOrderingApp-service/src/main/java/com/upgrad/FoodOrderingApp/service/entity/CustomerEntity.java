856d6cf5b7036857246944e0ddc20e380425bc91
Aditya Balasubramanyam <aditya.gnb@gmail.com>
2020-09-27 22:18:49 +0530

    Remove Not Null

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/CustomerEntity.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/CustomerEntity.java
@@ -43,7 +43,6 @@ public class CustomerEntity implements Serializable {
     private String firstName;
 
     @Column(name = "lastname")
-    @NotNull
     private String lastName;
 
     @Column(name = "email")
