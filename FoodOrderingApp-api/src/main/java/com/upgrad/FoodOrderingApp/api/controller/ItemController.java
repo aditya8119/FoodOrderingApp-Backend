0f7276d82928077174030b5fd567bbadef3d6702
soumyarout80 <soumyarout80@gmail.com>
2020-09-27 13:28:10 +0530

    Added item controller

--- FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/ItemController.java
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/ItemController.java
@@ -1,4 +1,34 @@
 package com.upgrad.FoodOrderingApp.api.controller;
 
+import com.upgrad.FoodOrderingApp.api.model.ItemList;
+import com.upgrad.FoodOrderingApp.service.businness.ItemService;
+import com.upgrad.FoodOrderingApp.service.businness.RestaurantService;
+import com.upgrad.FoodOrderingApp.service.entity.OrderEntity;
+import com.upgrad.FoodOrderingApp.service.entity.OrderItemEntity;
+import com.upgrad.FoodOrderingApp.service.entity.RestaurantEntity;
+import com.upgrad.FoodOrderingApp.service.exception.RestaurantNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class ItemController {
+
+    @Autowired
+    ItemService itemService;
+
+    @Autowired
+    RestaurantService restaurantService;
+
+    @CrossOrigin
+    @RequestMapping(method= RequestMethod.GET,value="/item/restaurant/{restaurant_id}",produces= MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<ItemList> getTopFiveItemsByRestaurantId(@PathVariable("restaurant_id") String restaurant_id) throws RestaurantNotFoundException {
+        RestaurantEntity restaurantEntity = restaurantService.getRestaurantById(restaurant_id);
+        itemService.getMostPopularItems(restaurant_id);
+        return null;
+    }
+
 }
